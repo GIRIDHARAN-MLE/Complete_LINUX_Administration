0ff2f2fcda59f25a0ba2174b667892e5cb305fef
root <root@ip-172-31-4-161.ap-south-1.compute.internal>
2023-10-01 08:57:01 +0000

    intro_2_linux

---
+++ Introdution_And_FileSystems_of_LINUX/Intro_2_UNIX_LINUX.sh
@@ -0,0 +1,2 @@
+
+			UNIX/LINUX is an Operating System which was first developed in the 1960s, and has been under constant development ever sine. By operating system, we mean the suite of programs which make the computer work. LINUX is a multi-user, multi-tasking operating system. We can have many users logged into a system simultaneously, each running many programs.
