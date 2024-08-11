dd1f47b248887bc0f07532b0bc74f1528b51a6f3
root <root@ip-172-31-35-177.ap-south-2.compute.internal>
2024-08-11 16:29:28 +0000

    LINUX_COMMANDS_

---
+++ LINUX_COMMANDS_BASIC_TO_ADVANCED/LINUX_Important_Commands.sh
@@ -0,0 +1,9 @@
+" The following commands are most important and useful for daily working in LINUX."
+
+* pwd :
+        It stands for 'print working directory'. It's a built-in command that displays the full
+        path-name of the current directory in the terminal.
+
+    For example :   [root@ip-172-31-35-*** LINUX_COMMANDS_BASIC_TO_ADVANCED]# pwd
+                    /root/Complete_LINUX_Administration/LINUX_COMMANDS_BASIC_TO_ADVANCED
+
